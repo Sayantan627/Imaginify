6720b24a9be2b9462bf3a3809e6c92ff139e901b
Sayantan_627 <27sayantan.pal@gmail.com>
2024-03-30 20:30:28 +0530

    Home page, search and pagination

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,46 @@
+import { Collection } from "@/components/shared/Collection";
+import { navLinks } from "@/constants";
+import { getAllImages } from "@/lib/actions/image.actions";
 import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 
-const Home = () => {
+const Home = async ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const searchQuery = (searchParams?.query as string) || "";
+
+  const images = await getAllImages({ page, searchQuery });
   return (
-    <div>
-      <p>Home</p>
-    </div>
+    <>
+      <section className="home">
+        <h1 className="home-heading">
+          Revamp Your Visuals Effortlessly with Imaginify
+        </h1>
+        <ul className="flex-center w-full gap-20">
+          {navLinks.slice(1, 5).map((link) => (
+            <Link
+              key={link.route}
+              href={link.route}
+              className="flex-center flex-col gap-2"
+            >
+              <li className="flex-center w-fit bg-white rounded-full p-4">
+                <Image src={link.icon} alt="image" height={24} width={24} />
+              </li>
+              <p className="p-14-medium text-center text-white">{link.label}</p>
+            </Link>
+          ))}
+        </ul>
+      </section>
+
+      <section className="sm:mt-12">
+        <Collection
+          hasSearch={true}
+          images={images?.data}
+          page={page}
+          totalPages={images?.totalPages}
+        />
+      </section>
+    </>
   );
 };
 export default Home;
